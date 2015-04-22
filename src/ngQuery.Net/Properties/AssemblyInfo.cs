0ddfd631c288acfb2f4af16b29b403d636810001
Stuart Blackler <stuart.blackler@4com.co.uk>
2015-04-22 14:00:41 +0100

    Initial Project Layout

---
+++ src/ngQuery.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ngQuery.Net")]
+[assembly: AssemblyDescription("Takes the JSON generated from ngQuery and parses it into an expression tree.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("4Com plc")]
+[assembly: AssemblyProduct("ngQuery.Net")]
+[assembly: AssemblyCopyright("Copyright © 4Com 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c94e7396-8e4e-4d00-a0ed-ce88afbe0ae6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
